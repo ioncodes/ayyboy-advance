34570d3b404e5ec799f1d58a3b78ae95764014ed
Luca Marcelli <luca@marcelli.ch>
2024-07-24 00:04:50 +0200

    lulw

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ mod memory;
 use arm7tdmi::cpu::Cpu;
 use memory::mmio::Mmio;
 
-const ARM_TEST: &[u8] = include_bytes!("../external/gba_bios.bin");
+const ARM_TEST: &[u8] = include_bytes!("../external/gba-tests/arm/arm.gba");
 
 fn main() {
     let mut mmio = Mmio::new();
     mmio.load(0, ARM_TEST);
 
     let mut cpu = Cpu::new();
+    cpu.registers.r[13] = 0x3007f00;
 
     loop {
         cpu.tick(&mut mmio);
